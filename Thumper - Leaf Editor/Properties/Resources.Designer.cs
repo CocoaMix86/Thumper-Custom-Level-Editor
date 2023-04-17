ae47c2e2d90aface9ba88a540c1c9bb3405f2031
CocoaMix 86 <austinpeters86@gmail.com>
2023-04-17 01:12:24 -0500

    updating namespace to "Thumper_Custom_Level_Editor"

--- Thumper - Leaf Editor/Properties/Resources.Designer.cs
+++ Thumper - Leaf Editor/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Thumper_Custom_Level_Editor.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Thumper___Leaf_Editor.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Thumper_Custom_Level_Editor.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
