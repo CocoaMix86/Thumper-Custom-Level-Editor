67e803ffa840a41b8c26ca2b7de2db4f690e0144
CocoaMix 86 <austinpeters86@gmail.com>
2023-09-02 15:00:15 -0500

    embedding resources

--- Thumper - Leaf Editor/Program.cs
+++ Thumper - Leaf Editor/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Thumper_Custom_Level_Editor
@@ -10,10 +15,37 @@ namespace Thumper_Custom_Level_Editor
 		/// </summary>
 		[STAThread]
 		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new FormLeafEditor());
-		}
-	}
+        {
+            // Force culture info, ensures periods . for decimals
+            var ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+            Application.Run(new FormLeafEditor());
+        }
+
+        static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+            var name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+
+            if (resources.Count() > 0) {
+                var resourceName = resources.First();
+
+                using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName)) {
+                    if (stream == null)
+                        return null;
+                    var block = new byte[stream.Length - 1 + 1];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+            }
+
+            return null;
+        }
+    }
 }
