26923d02db03020d32add7ba48abc96611e1e871
CocoaMix 86 <austinpeters86@gmail.com>
2023-10-02 11:59:28 -0500

    visual changes

--- Thumper - Leaf Editor/ThumperEditor.cs
+++ Thumper - Leaf Editor/ThumperEditor.cs
@@ -84,6 +84,7 @@ namespace Thumper_Custom_Level_Editor
             lvlToolStrip.Renderer = new ToolStripOverride();
             lvlVolumeToolStrip.Renderer = new ToolStripOverride();
             lvlPathsToolStrip.Renderer = new ToolStripOverride();
+            lvlLoopToolStrip.Renderer = new ToolStripOverride();
             toolstripTitleLvl.Renderer = new ToolStripOverride();
         }
         ///
