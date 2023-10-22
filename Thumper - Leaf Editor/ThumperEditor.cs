4d5e19a589a28969630a3864c5522d8b23749d43
CocoaMix <austinpeters86@gmail.com>
2023-10-22 16:43:58 -0500

    .

--- Thumper - Leaf Editor/ThumperEditor.cs
+++ Thumper - Leaf Editor/ThumperEditor.cs
@@ -911,6 +911,18 @@ namespace Thumper_Custom_Level_Editor
                 e.Graphics.DrawImage(Properties.Resources.icon_openedfolders, new Rectangle(x, y, w, h));
                 e.Handled = true;
             }
+            //button is in column 3, so that's where to draw the image
+            if (e.ColumnIndex == 3) {
+                e.Paint(e.CellBounds, DataGridViewPaintParts.All);
+                //get dimensions
+                var w = Properties.Resources.icon_remove2.Width;
+                var h = Properties.Resources.icon_remove2.Height;
+                var x = e.CellBounds.Left + (e.CellBounds.Width - w) / 2;
+                var y = e.CellBounds.Top + (e.CellBounds.Height - h) / 2;
+                //paint the image
+                e.Graphics.DrawImage(Properties.Resources.icon_remove2, new Rectangle(x, y, w, h));
+                e.Handled = true;
+            }
         }
         private void dgvRecentFiles_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -918,11 +930,21 @@ namespace Thumper_Custom_Level_Editor
                 return;
             //handle column 0 clicks only as that's where the button is
             if (e.ColumnIndex == 0) {
+                if (workingfolder == dgvRecentFiles.Rows[e.RowIndex].Cells[2].Value.ToString()) {
+                    panelRecentFiles.Visible = false;
+                    return;
+                }
                 //set working folder to the path
                 ClearPanels();
                 workingfolder = dgvRecentFiles.Rows[e.RowIndex].Cells[2].Value.ToString();
                 panelRecentFiles.Visible = false;
                 PlaySound("UIfolderclose");
+            }
+            //if remove column button clicked, run this
+            if (e.ColumnIndex == 3) {
+                dgvRecentFiles.Rows.RemoveAt(e.RowIndex);
+                Properties.Settings.Default.Recentfiles.RemoveAt(e.RowIndex);
+                Properties.Settings.Default.Save();
             }
         }
         private void btnRecentClose_Click(object sender, EventArgs e)
