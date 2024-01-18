582eb793dba2e6955fae7db2d028698f25dae332
CocoaMix <austinpeters86@gmail.com>
2024-01-17 19:30:23 -0600

    setting up function parsing

---
+++ Thumper - Leaf Editor/LevelDataClasses/CellFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thumper_Custom_Level_Editor
+{
+    public class CellFunction
+    {
+        public string function { get; set; }
+        public int rowindex { get; set; }
+        public int columnindex { get; set; }
+
+        public decimal Evaluate()
+        {
+            return 0;
+        }
+    }
+}
