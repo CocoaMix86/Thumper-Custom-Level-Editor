af37a09ab164eeaf183cdc4cefb129a15fa4d3dd
CocoaMix <austinpeters86@gmail.com>
2024-06-24 10:52:43 -0500

    adjustment for image help

--- Thumper - Leaf Editor/Other Forms/ImageMessageBox.cs
+++ Thumper - Leaf Editor/Other Forms/ImageMessageBox.cs
@@ -4,6 +4,8 @@ namespace Thumper_Custom_Level_Editor
 {
     public partial class ImageMessageBox : Form
 	{
+		private System.Drawing.Size _size;
+
 		public ImageMessageBox()
 		{
 			InitializeComponent();
@@ -15,18 +17,21 @@ namespace Thumper_Custom_Level_Editor
 				this.BackgroundImage = Properties.Resources.railcolorhelp;
 				this.BackgroundImageLayout = ImageLayout.Center;
 				this.Text = "Rail Color Explanation";
+				_size = this.BackgroundImage.Size;
 			}
 			if (path == "difficultyhelp") {
 				this.BackgroundImage = Properties.Resources.difficultyhelp;
-				this.BackgroundImageLayout = ImageLayout.Center;
+				this.BackgroundImageLayout = ImageLayout.Stretch;
 				this.Text = "Difficulty Explanation";
+				_size = new System.Drawing.Size(700, 690);
 			}
 			if (path == "bosssectionhelp") {
 				this.BackgroundImage = Properties.Resources.bosssectionhelp;
 				this.BackgroundImageLayout = ImageLayout.Center;
 				this.Text = "Boss Section Explanation";
+				_size = this.BackgroundImage.Size;
 			}
-			this.Size = this.BackgroundImage.Size;
+			this.Size = _size;
 			this.Height += 40;
 		}
 	}
