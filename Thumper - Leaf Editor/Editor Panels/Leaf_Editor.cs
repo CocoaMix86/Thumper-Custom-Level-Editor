f81a10028cc7ea539424e2e709fc6363b90ab04b
CocoaMix <austinpeters86@gmail.com>
2024-07-03 20:46:47 -0500

    fix copy sort order

--- Thumper - Leaf Editor/Editor Panels/Leaf_Editor.cs
+++ Thumper - Leaf Editor/Editor Panels/Leaf_Editor.cs
@@ -883,7 +883,7 @@ namespace Thumper_Custom_Level_Editor
 			clipboardtracks.Clear();
 			try {
                 List<DataGridViewRow> selectedrows = dgv.SelectedCells.Cast<DataGridViewCell>().Select(cell => cell.OwningRow).Distinct().ToList();
-				selectedrows.Sort((row, row2) => row2.Index.CompareTo(row.Index));
+				selectedrows.Sort((row, row2) => row.Index.CompareTo(row2.Index));
 				foreach (DataGridViewRow dgvr in selectedrows) {
 					clipboardtracks.Add(_tracks[dgvr.Index].Clone());
 				}
