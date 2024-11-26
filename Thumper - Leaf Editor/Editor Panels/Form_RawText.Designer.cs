ae42ed0abbe4134d6924507fbf4095a99b0aeca4
CocoaMix <austinpeters86@gmail.com>
2024-11-26 07:05:05 -0600

    raw

--- Thumper - Leaf Editor/Editor Panels/Form_RawText.Designer.cs
+++ Thumper - Leaf Editor/Editor Panels/Form_RawText.Designer.cs
@@ -28,59 +28,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_RawText));
-            this.textEditor = new Syncfusion.Windows.Forms.Edit.EditControl();
-            ((System.ComponentModel.ISupportInitialize)this.textEditor).BeginInit();
             this.SuspendLayout();
-            // 
-            // textEditor
-            // 
-            this.textEditor.AllowZoom = true;
-            this.textEditor.ChangedLinesMarkingLineColor = Color.FromArgb(255, 238, 98);
-            this.textEditor.CodeSnipptSize = new Size(100, 100);
-            this.textEditor.ContextChoiceBackColor = Color.FromArgb(255, 255, 255);
-            this.textEditor.ContextChoiceBorderColor = Color.FromArgb(233, 166, 50);
-            this.textEditor.ContextChoiceForeColor = SystemColors.InfoText;
-            this.textEditor.Dock = DockStyle.Fill;
-            this.textEditor.IndicatorMarginBackColor = Color.Empty;
-            this.textEditor.LikeVisualStudioSearch = true;
-            this.textEditor.LineNumbersColor = Color.FromArgb(0, 128, 128);
-            this.textEditor.Location = new Point(0, 0);
-            this.textEditor.Name = "textEditor";
-            this.textEditor.RenderRightToLeft = false;
-            this.textEditor.SelectionTextColor = Color.FromArgb(173, 214, 255);
-            this.textEditor.ShowEndOfLine = false;
-            this.textEditor.Size = new Size(539, 497);
-            this.textEditor.StatusBarSettings.CoordsPanel.Width = 150;
-            this.textEditor.StatusBarSettings.EncodingPanel.Width = 100;
-            this.textEditor.StatusBarSettings.FileNamePanel.Width = 100;
-            this.textEditor.StatusBarSettings.InsertPanel.Width = 33;
-            this.textEditor.StatusBarSettings.Offcie2007ColorScheme = Syncfusion.Windows.Forms.Office2007Theme.Blue;
-            this.textEditor.StatusBarSettings.Offcie2010ColorScheme = Syncfusion.Windows.Forms.Office2010Theme.Blue;
-            this.textEditor.StatusBarSettings.StatusPanel.Width = 70;
-            this.textEditor.StatusBarSettings.TextPanel.Width = 214;
-            this.textEditor.StatusBarSettings.VisualStyle = Syncfusion.Windows.Forms.Tools.Controls.StatusBar.VisualStyle.Default;
-            this.textEditor.TabIndex = 0;
-            this.textEditor.Text = "editControl1";
-            this.textEditor.UseXPStyleBorder = true;
-            this.textEditor.VScrollMode = Syncfusion.Windows.Forms.Edit.ScrollMode.Immediate;
-            this.textEditor.WrappedLinesOffset = 10;
-            this.textEditor.ZoomFactor = 1F;
             // 
             // Form_RawText
             // 
             this.BackColor = Color.FromArgb(31, 31, 31);
             this.ClientSize = new Size(539, 497);
-            this.Controls.Add(this.textEditor);
             this.ForeColor = Color.White;
             this.Icon = (Icon)resources.GetObject("$this.Icon");
             this.Name = "Form_RawText";
             this.Text = "Raw Editor";
-            ((System.ComponentModel.ISupportInitialize)this.textEditor).EndInit();
             this.ResumeLayout(false);
         }
 
         #endregion
-
-        public Syncfusion.Windows.Forms.Edit.EditControl textEditor;
     }
 }
